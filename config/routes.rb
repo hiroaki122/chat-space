3849fe096bbbc6e5a4b0a10d09fbbc4a25227184
hiroaki122 <jiro.h.v.122@gmail.com>
2019-02-20 23:45:25 +0900

    グループ機能実装
    j

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,8 @@
 Rails.application.routes.draw do
-  get 'users/edit'
-
-  get 'users/update'
-
   devise_for :users
-  root 'messages#index'
-  resources :users, only: [:edit, :update]
-  get 'messages' => 'messages#index'
-  
+  root 'groups#index'
+  resources :users, only: [:index, :edit, :update]
+  resources :groups, only: [:new, :create, :edit, :update] do
+    resources :messages, only: [:index, :create]
+  end
 end
