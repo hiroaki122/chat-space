aa20b7d7b23c288ff9de58aaadcd0f22681e062b
hiroaki122 <jiro.h.v.122@gmail.com>
2019-02-22 16:06:38 +0900

    コントローラー、モデル、ルーティング作成

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,9 @@ Rails.application.routes.draw do
 
   devise_for :users
   root 'messages#index'
-  resources :users, only: [:edit, :update]
-  get 'messages' => 'messages#index'
+  resources :users, only: [:edit, :update] do
+    resources :messages,only: [:index, :create]
+  end
+
   
 end
