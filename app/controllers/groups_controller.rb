57391ed0fd551ca5b9c0b5cbe51b284c8daf96a8
hiroaki122 <jiro.h.v.122@gmail.com>
2019-03-19 21:10:16 +0900

    コントローラー修正

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,6 +1,8 @@
 class GroupsController < ApplicationController
+
   before_action :set_group, only: [:edit, :update]
-  
+
+
   def index
   end
 
@@ -23,7 +25,7 @@ class GroupsController < ApplicationController
 
   def update
     if @group.update(group_params)
-       redirect_to group_messages_path(@group), :notice
+       redirect_to group_messages_path(@group)
     else
       render :edit
     end
